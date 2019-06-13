e23f51d4fc38d3394a278e5bf56eed6a23caee85
Kira <kira.mueller@outlook.com>
2019-06-12 19:17:21 -0700

    finalish version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,24 @@
 var gameObject = {
-    possibleWords: ['bulbasaur','ivysaur','venusaur','charmander','charmeleon','charizard','squirtle','wortortle','blastoise'],
+    // possibleWords: ['bulbasaur','ivysaur','venusaur','charmander','charmeleon','charizard','squirtle','wortortle','blastoise'],
+    possibleWords: ["bulbasaur","ivysaur","venusaur","charmander","charmeleon","charizard","squirtle","wartortle","blastoise","caterpie","metapod","butterfree",
+                    "weedle","kakuna","beedrill","pidgey","pidgeotto","pidgeot","rattata","raticate","spearow","fearow","ekans","arbok","pikachu","raichu",
+                    "sandshrew","sandslash","nidoran","nidorina","nidoqueen","nidoran","nidorino","nidoking","clefairy","clefable","vulpix","ninetales",
+                    "jigglypuff","wigglytuff","zubat","golbat","oddish","gloom","vileplume","paras","parasect","venonat","venomoth","diglett","dugtrio","meowth",
+                    "persian","psyduck","golduck","mankey","primeape","growlithe","arcanine","poliwag","poliwhirl","poliwrath","abra","kadabra","alakazam",
+                    "machop","machoke","machamp","bellsprout","weepinbell","victreebel","tentacool","tentacruel","geodude","graveler","golem","ponyta","rapidash",
+                    "slowpoke","slowbro","magnemite","magneton","farfetchd","doduo","dodrio","seel","dewgong","grimer","muk","shellder","cloyster","gastly",
+                    "haunter","gengar","onix","drowzee","hypno","krabby","kingler","voltorb","electrode","exeggcute","exeggutor","cubone","marowak","hitmonlee",
+                    "hitmonchan","lickitung","koffing","weezing","rhyhorn","rhydon","chansey","tangela","kangaskhan","horsea","seadra","goldeen","seaking",
+                    "staryu","starmie","mrmime","scyther","jynx","electabuzz","magmar","pinsir","tauros","magikarp","gyarados","lapras","ditto","eevee","vaporeon",
+                    "jolteon","flareon","porygon","omanyte","omastar","kabuto","kabutops","aerodactyl","snorlax","articuno","zapdos","moltres","dratini",
+                    "dragonair","dragonite","mewtwo","mew"],
     currentWord: [],
     wrongGuesses: [],
     guessedDisplay: [],
     wins: 0,
     losses: 0,
     remainingGuesses: 0,
+    gameOver: false,
 
     updateDisplay: function() {
         //TODO: write function to update relevant parts of the page once I have HTML set up
@@ -25,6 +38,7 @@ var gameObject = {
         this.wrongGuesses = [];
         this.guessedDisplay = [];
         this.remainingGuesses = 6;
+        this.gameOver = false;
 
         //set the currentWord array to a random element of the possibleWords array split into an array
         this.currentWord = this.possibleWords[Math.floor(Math.random() * this.possibleWords.length)].split('');
@@ -45,32 +59,40 @@ var gameObject = {
             }
         }
         else {
-            this.wrongGuesses.push(guess);
-            this.remainingGuesses--;
+            if (!(this.wrongGuesses.includes(guess))) {
+                this.wrongGuesses.push(guess);
+                this.remainingGuesses--;
+            }
+            
+        }
+
+
+        if (!(this.guessedDisplay.includes('_'))) {
+            // var tempWord = '';
+            // for (var i = 0; i < this.currentWord.length; i++) {
+            //     tempWord += this.currentWord[i];
+            // }
+
+            alert('Congrats, you won! Press any key to start a new game');
+            this.wins++;
+            this.gameOver = true;
+            // this.newGame();
+        }
+        else if (this.remainingGuesses <= 0) {
+            // var tempWord = '';
+            // for (var i = 0; i < this.currentWord.length; i++) {
+            //     tempWord += this.currentWord[i];
+            // }
+            this.guessedDisplay = this.currentWord;
+
+            alert('Oh no, you lost :( Press any key to start a new game');
+            this.losses++;
+            this.gameOver = true;
+            // this.newGame();
         }
 
         this.updateDisplay();
 
-        if (!(this.guessedDisplay.includes('_'))) {
-            var tempWord = '';
-            for (var i = 0; i < this.currentWord.length; i++) {
-                tempWord += this.currentWord[i];
-            }
-
-            alert('Congrats, you won! The word was ' + tempWord);
-            this.wins++;
-            this.newGame();
-        }
-        else if (this.remainingGuesses <= 0) {
-            var tempWord = '';
-            for (var i = 0; i < this.currentWord.length; i++) {
-                tempWord += this.currentWord[i];
-            }
-
-            alert('Oh no, you lost :( The word was ' + tempWord);
-            this.losses++;
-            this.newGame();
-        }
 
     },
 
@@ -88,7 +110,12 @@ var validKeys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
 gameObject.newGame();
 
 $(document).on('keyup', function(e) {
-    if (validKeys.includes(e.key)) {
+    if (gameObject.gameOver === true) {
+        gameObject.newGame();
+    }
+
+    else if (validKeys.includes(e.key)) {
         gameObject.checkGuess(e.key);
     }
+    
 });
